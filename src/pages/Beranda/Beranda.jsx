3a48c4261870ca31e22dbdbe3b6e81b12813c704
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-06-04 11:31:15 +0700

    Initial commit

---
+++ src/pages/Beranda/Beranda.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Beranda() {
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+      <Jadwal/>
+      <Category/>
+    </>
+  )
+}
+
+export default Beranda
