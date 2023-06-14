35084dd10e543ad63a48bcd3e4ed3dc7a141bc0b
OryzaAZ <zoryzaamaliatuz@gmail.com>
2023-06-14 20:32:18 +0700

    add modal flight from and add modal flight to

--- src/components/modal_beranda/ModalFlightFrom.jsx
+++ src/components/modal_beranda/ModalFlightFrom.jsx
@@ -3,60 +3,105 @@ import { useState } from "react";
 import { Dialog } from 'primereact/dialog';
 import { Divider } from 'primereact/divider';
 import { InputText } from "primereact/inputtext";
+import { Image } from "primereact/image";
 
-function ModalFlightFrom() {
+import icon_x from "../../assets/images/icon_x.svg"
+
+function ModalFlightFrom({ value, onSelect }) {
     const [visible, setVisible] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const [options] = useState([
+        "Jakarta (JKTA)",
+        "Bandung (BDG)",
+        "Surabaya (SBY)",
+        "Melbourne (MLB)"
+    ]);
+    const [recentSearches, setRecentSearches] = useState([]);
 
     const handleHapus = () => {
-        console.log("hapus ni")
+        setRecentSearches([]);
     }
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        const searchTerm = e.target.value.toLowerCase();
+        const filtered = options.filter(option => option.toLowerCase().includes(searchTerm));
+        setRecentSearches(filtered);
+        setSearchValue(searchTerm);
+    };
 
-        console.log("Melakukan pencarian:");
+    const handleSelection = (selectedValue) => {
+        onSelect(selectedValue);
+        setSearchValue("");
+        setVisible(false);
+    };
+
+    const handleRemoveSearch = (search) => {
+        const updatedSearches = recentSearches.filter(item => item !== search);
+        setRecentSearches(updatedSearches);
     };
 
     return (
         <>
-            <div className="pl-4 font-bold" onClick={() => setVisible(true)} style={{ cursor: "pointer" }}>
-                Jakarta(JKTA)
+            <div className="pl-4 font-bold cursor-pointer" onClick={() => setVisible(true)}>
+                {value}
             </div>
 
             <div className="card flex justify-content-center">
                 <Dialog
-                    header={<span className="p-input-icon-left">
-                        <i className="pi pi-search" onClick={handleSearch} />
-                        <InputText placeholder="Search" />
-                    </span>}
+                    header={
+                        <span className="p-input-icon-left">
+                            <i className="pi pi-search cursor-pointer" onClick={handleSearch} />
+                            <InputText
+                                placeholder="Search"
+                                value={searchValue}
+                                onChange={handleSearch}
+                            />
+                        </span>
+                    }
                     visible={visible}
                     modal={false}
                     style={{ width: '50vw' }}
-                    onHide={() => setVisible(false)}>
+                    onHide={() => setVisible(false)}
+                >
                     <div className="flex flex-col">
                         <div>
-                            <p className="pb-4 font-bold">Pencarian Terkini <span className="text-red-600 pl-96 pb-4" onClick={handleHapus}> Hapus</span></p>
+                            <p className="pb-4 font-bold">
+                                Pencarian Terkini{" "}
+                                <span
+                                    className="text-red-600 pl-96 pb-4 cursor-pointer"
+                                    onClick={handleHapus}
+                                >
+                                    Hapus
+                                </span>
+                            </p>
                         </div>
-                        <div className="">
-                            Jakarta
-                            <Divider />
-                        </div>
-                        <div>
-                            Bandung
-                            <Divider />
-                        </div>
-                        <div>
-                            Surabaya
-                            <Divider />
-                        </div>
+                        {recentSearches.map((search, index) => (
+                            <React.Fragment key={index}>
+                                <div className="flex items-center">
+                                    <p
+                                        className="pl-2 cursor-pointer"
+                                        onClick={() => handleSelection(search)}
+                                    >
+                                        {search}
+                                    </p>
+                                    <div className="flex ml-auto mr-10">
+                                        <Image
+                                            src={icon_x}
+                                            alt="Remove"
+                                            onClick={() => handleRemoveSearch(search)}
+                                            className="cursor-pointer"
+                                        />
+                                    </div>
+                                </div>
+                                <Divider style={{ margin: '8px 0' }} />
+                            </React.Fragment>
+                        ))}
                     </div>
                 </Dialog>
             </div>
-
         </>
-
-
-    )
-
+    );
 }
+
 
 export default ModalFlightFrom;
